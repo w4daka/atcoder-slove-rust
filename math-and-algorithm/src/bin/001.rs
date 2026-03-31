97ae6ab56dd4eeb4022ec49730c198d6abb504d7
w4daka <w4daka999@gmail.com>
2026-03-31 14:36:13 +0900

    feat(001.rs)add

--- math-and-algorithm/src/bin/001.rs
+++ math-and-algorithm/src/bin/001.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    todo!();
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).unwrap();
+
+    let mikan: u64 = input.trim().parse().unwrap();
+
+    let apple: u64 = 5;
+    let ans = apple + mikan;
+    println!("{}", ans)
 }
